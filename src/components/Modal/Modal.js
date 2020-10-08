f1d3d7950214f66b9517b8997e1e421ba7dea194
vijeth k gowda <vijethkgowda@gmail.com>
2020-10-08 15:45:44 +0530

    calender changes

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -44,72 +44,74 @@ const Modal = ({ closeModal, userActiveTimes }) => {
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="9" x2="15" y2="15"></line><line x1="15" y1="9" x2="9" y2="15"></line></svg>
         </button>
 
-        <div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 px-4 py-2">
-            <div className="w-full pr-2 md:pr-4">
-              <span>
-                <h2 className="text-center text-xl font-bold text-gray-800 pb-5">
-                  User Periods of Activity <span className="text-xs font-hairline"></span>
-                </h2>
-              </span>
-              {
-                (activeTimeResult1 != '') ? (
-                  <div className="overflow-y-auto h-64">
-                    <table className="table-auto border-collapse w-full p-2">
-                      <thead>
-                        <tr className="rounded-lg text-sm font-medium text-gray-700 text-left bg-gray-200">
-                          <th className="px-4 py-3">Date</th>
-                          <th className="px-4 py-3">From</th>
-                          <th className="px-4 py-3">To</th>
-                        </tr>
-                      </thead>
-                      {activeTimeResult1.map((time, index) => (
-                        <tbody className="text-sm font-normal text-gray-700 " key={time.start_time}>
-                          <tr className={`${index == 0 || index % 2 == 0 ? 'bg-white' : 'bg-gray-200'} rounded py-4`}>
-
-                            <td className="px-4 py-3">{time.start_time.slice(0, 11)}</td>
-                            <td className="px-4 py-3">{time.start_time.slice(12,)}</td>
-                            <td className="px-4 py-3">{time.end_time.slice(12,)}</td>
-                          </tr>
-                        </tbody>
-                      ))}
-                    </table>
-                  </div>
-
-                ) : (
-                    <h2 className="text-center text-base font-bold text-red-600 pb-5">
-                      No Record Found
-                    </h2>)
-              }
-            </div>
-            <div className="max-w-sm mt-5">
-              <Calendar
-                colorPrimaryLight
-                calendarTodayClassName="custom-today-day"
-                value={selectedDays}
-                onChange={clickHandler}
-                shouldHighlightWeekends
-                colorPrimary="#4299e1"
-                calendarClassName="custom-calendar"
-                calendarTodayClassName="custom-today-day" 
-                renderFooter={() => (
-                  <div className="w-full flex justify-center">
-                    <button
-                      className="px-4 py-2 bg-red-500 text-sm font-semibold text-white rounded"
-                      style={{ 'border': 'none', 'outline': 'none' }}
-                      onClick={() => {
-                        clickHandler
-                        setSelectedDays(null)
-                        setActiveTimeResult(userActiveTimes)
-                      }}
-                    >
-                      Reset Date!
+        <div className="grid grid-cols-1 sm:grid-cols-2 px-4 py-2 mt-4">
+          <div className="max-w-sm mt-5">
+            <Calendar
+              colorPrimaryLight
+              calendarTodayClassName="custom-today-day"
+              value={selectedDays}
+              onChange={clickHandler}
+              shouldHighlightWeekends
+              colorPrimary="#4299e1"
+              calendarClassName="custom-calendar"
+              calendarTodayClassName="custom-today-day"
+              renderFooter={() => (
+                <div className="w-full flex justify-center">
+                  <button
+                    className="px-4 py-2 bg-red-500 text-sm font-semibold text-white rounded m-4"
+                    style={{ 'border': 'none', 'outline': 'none' }}
+                    onClick={() => {
+                      clickHandler
+                      setSelectedDays(null)
+                      setActiveTimeResult(userActiveTimes)
+                    }}
+                  >
+                    Reset Date!
                     </button>
-                  </div>
-                )}
-              />
-            </div>
+                </div>
+              )}
+            />
           </div>
+
+
+          <div className="w-full pr-2 md:pr-4">
+            <span>
+              <h2 className="text-center text-xl font-bold text-gray-800 pb-5">
+                User Periods of Activity <span className="text-xs font-hairline"></span>
+              </h2>
+            </span>
+            {
+              (activeTimeResult1 != '') ? (
+                <div className="overflow-y-auto h-72">
+                  <table className="table-auto border-collapse w-full p-2">
+                    <thead>
+                      <tr className="rounded-lg text-sm font-medium text-gray-700 text-left bg-gray-200">
+                        <th className="px-4 py-3">Date</th>
+                        <th className="px-4 py-3">From</th>
+                        <th className="px-4 py-3">To</th>
+                      </tr>
+                    </thead>
+                    {activeTimeResult1.map((time, index) => (
+                      <tbody className="text-sm font-normal text-gray-700 " key={time.start_time}>
+                        <tr className={`${index == 0 || index % 2 == 0 ? 'bg-white' : 'bg-gray-200'} rounded py-4`}>
+
+                          <td className="px-4 py-3">{time.start_time.slice(0, 11)}</td>
+                          <td className="px-4 py-3">{time.start_time.slice(12,)}</td>
+                          <td className="px-4 py-3">{time.end_time.slice(12,)}</td>
+                        </tr>
+                      </tbody>
+                    ))}
+                  </table>
+                </div>
+
+              ) : (
+                  <h2 className="text-center text-base font-bold text-red-600 pb-5">
+                    No Record Found
+                  </h2>)
+            }
+          </div>
+
+
         </div>
       </div>
     </div>
